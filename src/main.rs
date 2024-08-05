ed6bad1b96675059b45cdc0426641b7f73af9e38
kul-sudo <ulyanov.kirill@protonmail.com>
2024-08-06 00:00:42 +0300

    fmt: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use comrak::{Arena, parse_document, Options};
+use comrak::{parse_document, Arena, Options};
 
 fn main() {
     let content = {
@@ -11,11 +11,11 @@ fn main() {
 
     println!("<body style=\"background-color: #121212; color: #e0e0e0;\">");
     println!("{}", parse_block(&root));
-}  
+}
 
-use comrak::nodes::{AstNode, NodeValue, NodeCodeBlock, NodeCode};
-use comrak::plugins::syntect::{SyntectAdapter, SyntectAdapterBuilder};
 use comrak::adapters::SyntaxHighlighterAdapter;
+use comrak::nodes::{AstNode, NodeCode, NodeCodeBlock, NodeValue};
+use comrak::plugins::syntect::{SyntectAdapter, SyntectAdapterBuilder};
 
 use syntect::parsing::{SyntaxSet, SyntaxSetBuilder};
 
@@ -34,14 +34,16 @@ static ADAPTER: LazyLock<SyntectAdapter> = LazyLock::new(|| {
 
 fn parse_block<'a>(node: &'a AstNode<'a>) -> String {
     match node.data.borrow().value {
-        NodeValue::Heading(_)     => format!("\n<h2>{}</h2>\n", to_string(node)),
+        NodeValue::Heading(_) => format!("\n<h2>{}</h2>\n", to_string(node)),
         NodeValue::Text(ref text) => String::from(text),
-        NodeValue::LineBreak      => String::from("<br>\n"),
-        NodeValue::Paragraph      => format!("{}\n", to_string(node)),
-        NodeValue::BlockQuote     => format!("\n<div class=\"block quote\">{}</div>\n", to_string(node)),
-        NodeValue::Strong         => format!("<b>{}</b>", to_string(node)),
-        NodeValue::Emph           => format!("<i>{}</i>", to_string(node)),
-        NodeValue::Strikethrough  => format!("<s>{}</s>", to_string(node)),
+        NodeValue::LineBreak => String::from("<br>\n"),
+        NodeValue::Paragraph => format!("{}\n", to_string(node)),
+        NodeValue::BlockQuote => {
+            format!("\n<div class=\"block quote\">{}</div>\n", to_string(node))
+        }
+        NodeValue::Strong => format!("<b>{}</b>", to_string(node)),
+        NodeValue::Emph => format!("<i>{}</i>", to_string(node)),
+        NodeValue::Strikethrough => format!("<s>{}</s>", to_string(node)),
         NodeValue::HtmlInline(ref html) => String::from(html),
 
         NodeValue::Code(NodeCode { ref literal, .. }) => format!("<c>{literal}</c>"),
@@ -52,8 +54,11 @@ fn parse_block<'a>(node: &'a AstNode<'a>) -> String {
                 .write_highlighted(&mut out, Some("shard"), literal)
                 .unwrap();
 
-            format!("<code><pre>\n{}</pre></code>\n", String::from_utf8(out).unwrap())
-        },
+            format!(
+                "<code><pre>\n{}</pre></code>\n",
+                String::from_utf8(out).unwrap()
+            )
+        }
 
         _ => to_string(node),
     }
@@ -62,4 +67,3 @@ fn parse_block<'a>(node: &'a AstNode<'a>) -> String {
 fn to_string<'a>(node: &'a AstNode<'a>) -> String {
     node.children().map(parse_block).collect::<String>()
 }
-
