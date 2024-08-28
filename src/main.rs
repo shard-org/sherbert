79b0bd5ff65ed5d6920bb75046798a959a2ef785
slbsh <crafter.antek@gmail.com>
2024-08-28 15:17:58 +0200

    change: better colour definitions

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn main() {
 
     println!(
 "<!DOCTYPE html>
-<body style=\"background-color: #1d2021; color: #ebdbb2\">
-");
+<body style=\"background-color: #{:x}; color: #{:x}\">
+", COLOUR[0], COLOUR[2]);
     println!("{}", parse_block(root));
 
 }
@@ -27,25 +27,42 @@ use syntect::parsing::{SyntaxSetBuilder, Scope, ScopeStack};
 use std::sync::LazyLock;
 use std::collections::BTreeMap;
 
+// gruvbox
+const COLOUR: [u32; 13] = [
+    0x1d2021, // background
+    0x928375, // comments
+    0xebdbb2, // foreground
+    0xebdbb2, // separators
+    0xfe8019, // types
+    0xfb4934, // functions, mutation, declaration
+    0xb8bb26, // keywords
+    0x83a598, // operators
+    0x689d6b, // tags
+    0x8ec07c, // macros
+    0xfabd2f, // builtins
+    0xb8bb27, // strings
+    0xd3869b, // numbers
+];
+
+// // nord
+// const COLOUR: [u32; 13] = [
+//     0x2E3440, // background
+//     0x4C566A, // comments
+//     0xD8DEE9, // foreground
+//     0xECEFF4, // separators
+//     0x8FBCBB, // types
+//     0x88C0D0, // functions, mutation, declaration
+//     0x81A1C1, // keywords
+//     0x81A1C1, // operators
+//     0x5E81AC, // tags
+//     0x5E81AC, // macros
+//     0xD08770, // builtins
+//     0xA3BE8C, // strings
+//     0xB48EAD, // numbers
+// ];
+
+
 static ADAPTER: LazyLock<SyntectAdapter> = LazyLock::new(|| {
-    let mut set = SyntaxSetBuilder::new();
-    set.add_plain_text_syntax();
-    set.add_from_folder("syntax/", true).unwrap();
-
-    SyntectAdapterBuilder::new()
-        .theme_set(ThemeSet {
-            themes: {
-                let mut themes = BTreeMap::new();
-                themes.insert("gruvbox".to_string(), THEME.clone());
-                themes
-            }
-        })
-        .theme("gruvbox")
-        .syntax_set(set.build())
-        .build()
-});
-
-static THEME: LazyLock<Theme> = LazyLock::new(|| {
     let color = |hex: u32| Color {
         r: (hex >> 16) as u8,
         g: (hex >> 8) as u8,
@@ -54,7 +71,7 @@ static THEME: LazyLock<Theme> = LazyLock::new(|| {
     };
 
     let item = |path: &str, hex: u32, style: Option<FontStyle>| ThemeItem {
-        scope: ScopeSelectors { selectors: vec![ScopeSelector{ path: ScopeStack::from_vec(vec![Scope::new(path).unwrap()]), excludes: Vec::new()}]},
+        scope: ScopeSelectors { selectors: vec![ScopeSelector { path: ScopeStack::from_vec(vec![Scope::new(path).unwrap()]), excludes: Vec::new()}]},
         style: StyleModifier {
             foreground: Some(color(hex)),
             background: None,
@@ -62,47 +79,67 @@ static THEME: LazyLock<Theme> = LazyLock::new(|| {
         },
     };
 
-    Theme {
-        name:     Some(String::from("gruvbox")),
+    let theme = Theme {
+        name:     Some(String::from("theme")),
         author:   None,
         settings: ThemeSettings {
-            foreground: Some(color(0xebdbb2)),
+            foreground: Some(color(COLOUR[2])),
             ..Default::default()
         },
         scopes:   vec![
-            item("comment",           0x928375, None),
+            item("comment",            COLOUR[1],  None),
 
-            item("literal.string",    0xb8bb27, None),
-            item("literal.float",     0xd3869b, None),
-            item("literal.integer",   0xd3869b, None),
+            item("literal.string",     COLOUR[11], None),
+            item("literal.float",      COLOUR[12], None),
+            item("literal.integer",    COLOUR[12], None),
 
-            item("keyword.ret",       0xcc241e, Some(FontStyle::BOLD)),
-            item("keyword.other",     0xb8bb26, Some(FontStyle::BOLD)),
-            item("keyword.attribute", 0x8ec07c, Some(FontStyle::ITALIC)),
+            item("keyword.ret",        COLOUR[6],  Some(FontStyle::BOLD)),
+            item("keyword.other",      COLOUR[6],  Some(FontStyle::BOLD)),
+            item("keyword.attribute",  COLOUR[6],  Some(FontStyle::ITALIC)),
 
-            item("tag.inner",         0x689d6b, Some(FontStyle::BOLD)),
-            item("tag.outer",         0x689d6b, Some(FontStyle::BOLD)),
+            item("tag.inner",          COLOUR[8],  Some(FontStyle::BOLD)),
+            item("tag.outer",          COLOUR[8],  Some(FontStyle::BOLD)),
 
-            item("preprocess.macro",  0x8ec07c, Some(FontStyle::BOLD)),
+            item("preprocess.macro",   COLOUR[9],  Some(FontStyle::BOLD)),
 
-            item("entity.type",       0xfe8019, Some(FontStyle::BOLD)),
-            item("entity.type.lesser",0xfe8019, Some(FontStyle::ITALIC)),
-            item("entity.function",   0xfb4934, None),
+            item("entity.type",        COLOUR[4],  Some(FontStyle::BOLD)),
+            item("entity.type.lesser", COLOUR[4],  Some(FontStyle::ITALIC)),
+            item("entity.function",    COLOUR[5],  None),
 
-            item("builtin.call",      0xfabd2f, Some(FontStyle::BOLD | FontStyle::ITALIC)),
-            item("builtin.call",      0xfabd2f, Some(FontStyle::BOLD)),
+            item("builtin.call",       COLOUR[10], Some(FontStyle::BOLD | FontStyle::ITALIC)),
+            item("builtin.outer",      COLOUR[10], Some(FontStyle::BOLD)),
 
-            item("op.mutation",       0xfb4934, None),
-            item("op.declaration",    0xfb4934, None),
-            item("op.arithmetic",     0x83a598, None),
-            item("op.bitwise",        0x83a598, None),
-            item("op.assignment",     0x83a598, None),
-            item("op.logic",          0x83a598, None),
-            item("op.other",          0x83a598, None),
-            item("op.type",           0xfe8019, Some(FontStyle::BOLD)),
-            item("op.brackets",       0xfe8019, None),
+            item("op.mutation",        COLOUR[5],  None),
+            item("op.declaration",     COLOUR[5],  None),
+            item("op.arithmetic",      COLOUR[7],  None),
+            item("op.bitwise",         COLOUR[7],  None),
+            item("op.assignment",      COLOUR[7],  None),
+            item("op.logic",           COLOUR[7],  None),
+            item("op.other",           COLOUR[7],  None),
+            item("op.type",            COLOUR[4],  Some(FontStyle::BOLD)),
+            item("op.ref",             COLOUR[4],  None),
+            item("op.brackets",        COLOUR[4],  None),
+            item("op.semicolon",       COLOUR[3],  None),
+
+            item("syntax.separator",   COLOUR[3],  None),
         ],
-    }
+    };
+
+    let mut set = SyntaxSetBuilder::new();
+    set.add_plain_text_syntax();
+    set.add_from_folder("syntax/", true).unwrap();
+
+    SyntectAdapterBuilder::new()
+        .theme_set(ThemeSet {
+            themes: {
+                let mut themes = BTreeMap::new();
+                themes.insert("theme".to_string(), theme);
+                themes
+            }
+        })
+        .theme("theme")
+        .syntax_set(set.build())
+        .build()
 });
 
 fn parse_block<'a>(node: &'a AstNode<'a>) -> String {
